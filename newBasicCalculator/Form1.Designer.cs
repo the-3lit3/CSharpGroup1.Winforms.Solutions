0a31fb27d5dfb31e493626dd332a3f44648e56ed
S. Facey Fotos <S. Facey Fotos@LAPTOP-K0D1IIFU>
2022-02-26 19:15:48 -0500

    new commit

---
+++ newBasicCalculator/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace newBasicCalculator
+{
+    partial class basicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblResultDisplay = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.txtMessagebox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Location = new System.Drawing.Point(57, 65);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(52, 16);
+            this.lblNumber1.TabIndex = 0;
+            this.lblNumber1.Text = "Value 1";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Location = new System.Drawing.Point(57, 104);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(52, 16);
+            this.lblNumber2.TabIndex = 1;
+            this.lblNumber2.Text = "Value 2";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(57, 172);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 16);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Result";
+            // 
+            // lblResultDisplay
+            // 
+            this.lblResultDisplay.AutoSize = true;
+            this.lblResultDisplay.BackColor = System.Drawing.Color.Silver;
+            this.lblResultDisplay.Location = new System.Drawing.Point(140, 172);
+            this.lblResultDisplay.Name = "lblResultDisplay";
+            this.lblResultDisplay.Size = new System.Drawing.Size(124, 16);
+            this.lblResultDisplay.TabIndex = 3;
+            this.lblResultDisplay.Text = "                                       ";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(142, 62);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(124, 22);
+            this.txtNumber1.TabIndex = 4;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(143, 98);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(124, 22);
+            this.txtNumber2.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(142, 126);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(22, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(176, 126);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(22, 23);
+            this.btnMinus.TabIndex = 7;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(210, 126);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(22, 23);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "x";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(244, 126);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(22, 23);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // txtMessagebox
+            // 
+            this.txtMessagebox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtMessagebox.Location = new System.Drawing.Point(308, 63);
+            this.txtMessagebox.Multiline = true;
+            this.txtMessagebox.Name = "txtMessagebox";
+            this.txtMessagebox.ReadOnly = true;
+            this.txtMessagebox.Size = new System.Drawing.Size(224, 125);
+            this.txtMessagebox.TabIndex = 10;
+            this.txtMessagebox.Text = "Basic Calculator that takes two (2) numbers\r\nas input, performs the necessary\r\nca" +
+    "lculation and return a correct result.";
+            this.txtMessagebox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // basicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMessagebox);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lblResultDisplay);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Name = "basicCalculator";
+            this.Text = "Basic Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblResultDisplay;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.TextBox txtMessagebox;
+    }
+}
+
---
+++ newBasicCalculator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newBasicCalculator
+{
+    public partial class basicCalculator : Form
+    {
+        
+        public basicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDouble(txtNumber1.Text);
+            var num2 = Convert.ToDouble(txtNumber2.Text);
+          var sum = num1 + num2;
+            lblResultDisplay.Text = sum.ToString();
+            btnAdd.Visible = false;
+            btnDivide.Visible = true;
+  
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDouble(txtNumber1.Text);
+            var num2 = Convert.ToDouble(txtNumber2.Text);
+
+            var difference = num1 - num2;
+            lblResultDisplay.Text = difference.ToString();
+            btnMinus.Visible = false;
+            btnAdd.Visible = true;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDouble(txtNumber1.Text);
+            var num2 = Convert.ToDouble(txtNumber2.Text);
+
+            var product = num1 * num2;
+            lblResultDisplay.Text = product.ToString();
+            btnMultiply.Visible = false;
+            btnMinus.Visible = true;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            var num1 = Convert.ToDouble(txtNumber1.Text);
+            var num2 = Convert.ToDouble(txtNumber2.Text);
+
+            var quotient = num1 / num2;
+            lblResultDisplay.Text = quotient.ToString();
+            btnDivide.Visible = false;
+            btnMultiply.Visible = true;
+        }
+    }
+    
+}
